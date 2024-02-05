2b8283a06692724f6b96d40712af41e2534be70a
saz-im-netz <130750824+saz-im-netz@users.noreply.github.com>
2024-02-05 16:37:48 +0100

    get all data back

--- data.js
+++ data.js
@@ -6,69 +6,69 @@ export const dogs = [
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
         hasBeenLiked: false
+    },
+    {
+        name: "Bella",
+        avatar: "images/dog-bella.jpg",
+        age: 43,
+        bio: "Yup, that's my owner. U can meet him if you want",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Teddy",
+        avatar: "images/dog-teddy.jpg",
+        age: 30,
+        bio: "How you doin?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Pakkun",
+        avatar: "images/dog-pakkun.jpg",
+        age: 21,
+        bio: "Wanna feel my paw? It's really soft.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Bofrost",
+        avatar: "images/dog-bofrost.jpg",
+        age: 17,
+        bio: "Always very chill!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Miles",
+        avatar: "images/dog-miles.jpg",
+        age: 24,
+        bio: "I got a flower for you.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Jamie",
+        avatar: "images/dog-jamie.jpg",
+        age: 19,
+        bio: "Look at this!",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "Carster",
+        avatar: "images/dog-carsten.jpg",
+        age: 27,
+        bio: "Wanna go to the club?",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
+    },
+    {
+        name: "James",
+        avatar: "images/dog-james.jpg",
+        age: 23,
+        bio: "Let's grab a bite together.",
+        hasBeenSwiped: false,
+        hasBeenLiked: false
     }
-    // {
-    //     name: "Bella",
-    //     avatar: "images/dog-bella.jpg",
-    //     age: 43,
-    //     bio: "Yup, that's my owner. U can meet him if you want",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Teddy",
-    //     avatar: "images/dog-teddy.jpg",
-    //     age: 30,
-    //     bio: "How you doin?",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Pakkun",
-    //     avatar: "images/dog-pakkun.jpg",
-    //     age: 21,
-    //     bio: "Wanna feel my paw? It's really soft.",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Bofrost",
-    //     avatar: "images/dog-bofrost.jpg",
-    //     age: 17,
-    //     bio: "Always very chill!",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Miles",
-    //     avatar: "images/dog-miles.jpg",
-    //     age: 24,
-    //     bio: "I got a flower for you.",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Jamie",
-    //     avatar: "images/dog-jamie.jpg",
-    //     age: 19,
-    //     bio: "Look at this!",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "Carster",
-    //     avatar: "images/dog-carsten.jpg",
-    //     age: 27,
-    //     bio: "Wanna go to the club?",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // },
-    // {
-    //     name: "James",
-    //     avatar: "images/dog-james.jpg",
-    //     age: 23,
-    //     bio: "Let's grab a bite together.",
-    //     hasBeenSwiped: false,
-    //     hasBeenLiked: false
-    // }
 ]
