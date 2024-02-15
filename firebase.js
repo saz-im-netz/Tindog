ba4a796b06a31492c742ef6ab2055baa5f934537
saz-im-netz <130750824+saz-im-netz@users.noreply.github.com>
2024-02-15 14:26:05 +0100

    add firebase started

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzC2O6lKzvuaY7zbwZjeEK-x-bkVMVf34",
+  authDomain: "tindog-eafaf.firebaseapp.com",
+  projectId: "tindog-eafaf",
+  storageBucket: "tindog-eafaf.appspot.com",
+  messagingSenderId: "868220778537",
+  appId: "1:868220778537:web:43afad30e7ea57aec48438"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
